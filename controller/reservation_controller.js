11b324e70911f21cb7006b3bca5f87ef7305eac9
PaulineHavez <131375628+PaulineHavez@users.noreply.github.com>
2023-06-29 10:16:25 +0200

    modeles participants et réservation

---
+++ controller/reservation_controller.js
@@ -0,0 +1,3 @@
+const Reservation = require("../models/Reservation");
+const express = require("express");
+const utilisateurRouter = express.Router();
---
+++ models/Participant.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// creer schema pour Participant
+const schemaParticipant = new mongoose.Schema({
+  //   salleId: String,
+  //   jour: String,
+  //   creneau: String,
+  //   utilisateurId: String,
+});
+
+// creer le model pour Participant
+const Participant = mongoose.model("participants", schemaParticipant);
+
+module.exports = Participant;
