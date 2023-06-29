61645edbc2d37c62ed2fc335ef12c7d3e3a126e0
PaulineHavez <131375628+PaulineHavez@users.noreply.github.com>
2023-06-29 10:25:33 +0200

    modele participant

--- models/Participant.js
+++ models/Participant.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 // creer schema pour Participant
 const schemaParticipant = new mongoose.Schema({
-  //   salleId: String,
-  //   jour: String,
-  //   creneau: String,
-  //   utilisateurId: String,
+  salleId: String,
+  jour: String,
+  creneau: String,
+  utilisateurId: String,
 });
 
 // creer le model pour Participant
