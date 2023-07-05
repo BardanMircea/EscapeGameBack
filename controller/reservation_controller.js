4030af6654fb5c0f8875472b3528cff012969143
PaulineHavez <131375628+PaulineHavez@users.noreply.github.com>
2023-07-05 09:54:55 +0200

    route get pour historique

--- controller/reservation_controller.js
+++ controller/reservation_controller.js
@@ -2,8 +2,11 @@ const Reservation = require("../models/Reservation");
 const express = require("express");
 const reservationRouter = express.Router();
 const mongoose = require("mongoose");
+const Utilisateur = require("../models/Utilisateur");
+const Participant = require("../models/Participant");
+const Salle = require("../models/Salle");
 
-// middleware to check valid id reservation 
+// middleware to check valid id reservation
 const checkValidIdReserv = async (req, res, next) => {
   try {
     const id = req.params.id;
@@ -35,6 +38,47 @@ reservationRouter.get("/:id", checkValidIdReserv, async (req, res) => {
   let id = req.params.id;
   const reservation = await Reservation.findOne({ _id: id });
   res.json(reservation);
+});
+
+// //get reservation by utilisateur id
+reservationRouter.get("/utilisateur/:id", async (req, res) => {
+  let idUser = req.params.id;
+
+  const reservation = await Reservation.find({ utilisateurId: idUser });
+  if (reservation) {
+    res.status(201).json(reservation);
+  } else {
+    return res
+      .status(404)
+      .json({ error: "Cet utilisateur n'a pas de réservation" });
+  }
+});
+
+// //get PARTICIPANTS reservation by utilisateur id
+reservationRouter.get("/participants/:id", async (req, res) => {
+  let idUser = req.params.id;
+  const reservations = await Reservation.find({ utilisateurId: idUser });
+  if (!reservations) {
+    return res
+      .status(404)
+      .json({ error: "Cet utilisateur n'a pas de réservation" });
+  }
+  let bookedRoom = await Promise.all(
+    reservations.map(async (reservation) => {
+      const participants = await Participant.find({
+        reservationId: reservation._id,
+      });
+      const salle = await Salle.find({ _id: reservation.salleId });
+      return {
+        reservation: reservation,
+        participants: participants,
+        salle: salle[0],
+      };
+    })
+  );
+  if (bookedRoom) {
+    res.status(201).json(bookedRoom);
+  }
 });
 
 // Create a reservation
