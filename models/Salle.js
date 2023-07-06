758791676d1748009ea1891919a96ded98dc7dff
PaulineHavez <131375628+PaulineHavez@users.noreply.github.com>
2023-07-06 20:46:29 +0200

    modele Salle

--- models/Salle.js
+++ models/Salle.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 // creer le schema pour Salle
 const schemaSalle = new mongoose.Schema({
-    nom : String,
-    description : String,
-    img : String,
-    capacite : String
-})
+  nom: String,
+  description: String,
+  img: String,
+  capacite: String,
+  status: Boolean,
+});
 
 // creer le modele pour Salle
 const Salle = mongoose.model("salles", schemaSalle);
 
-module.exports = Salle
+module.exports = Salle;
