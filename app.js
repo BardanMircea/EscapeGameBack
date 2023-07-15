8df9905d9fc9da81571cfb0728df35468d3b1233
BardanMircea <mircea.c.bardan@gmail.com>
2023-07-15 20:45:25 +0300

    Update app.js to hide mongoDb credentials

--- app.js
+++ app.js
@@ -36,11 +36,3 @@ app.listen(port, async () => {
   await mongoose.connect(process.env.MONGODB_URI); // et passer cette variable comme paramettre dans la methode connect()
   console.log(`App started on port ${port}`);
 });
-
-// L'alternative serait de nous nous connecter directement, sans [.env], au MongoDb, comme suit, mais cela va exposer notre mdp a tout le monde ayant access a notre Repo GitHub:
-
-// app.listen(port, async () =>{
-//     const MONGODB_URI="mongodb+srv://mirceacbardan:dictateurs2023@cluster0.w7mwvau.mongodb.net/EscapeGame?retryWrites=true&w=majority"
-//     await mongoose.connect(MONGODB_URI)
-//     console.log(`App started on port ${port}`)
-// })
